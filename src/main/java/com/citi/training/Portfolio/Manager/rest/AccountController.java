c586fa3034e8c99b5de43457ca5dc5d7c9004d21
Katie Swerdloff <c.swerdloff@ufl.edu>
2021-08-13 18:58:00 +0000

    added get all accounts

--- src/main/java/com/citi/training/Portfolio/Manager/rest/AccountController.java
+++ src/main/java/com/citi/training/Portfolio/Manager/rest/AccountController.java
@@ -2,10 +2,12 @@ package com.citi.training.Portfolio.Manager.rest;
 
 import com.citi.training.Portfolio.Manager.entities.Account;
 import com.citi.training.Portfolio.Manager.service.AccountService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -15,8 +17,11 @@ public class AccountController {
 
     @Autowired
     private AccountService accountService;
-    @GetMapping
-    public Account getAccount(){
-        return accountService.getAccountById(1);
+
+    @RequestMapping(method = RequestMethod.GET)
+    public Iterable<Account> getAccounts(){
+
+        return accountService.getAllAccounts();
+
     }
 }
--- src/main/java/com/citi/training/Portfolio/Manager/service/AccountServiceImpl.java
+++ src/main/java/com/citi/training/Portfolio/Manager/service/AccountServiceImpl.java
@@ -4,6 +4,8 @@ import com.citi.training.Portfolio.Manager.entities.Account;
 import com.citi.training.Portfolio.Manager.repo.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -13,11 +15,18 @@ public class AccountServiceImpl implements AccountService {
     @Autowired
     private AccountRepository accountRepository;
 
+    @Override
     public Account getAccountById(int id){
         Optional<Account> accOptional =  accountRepository.findById(id);
         if (accOptional.isPresent()) {
             return accOptional.get();
         }
         else return null;
+    }
+
+    @Override
+    @Transactional(propagation = Propagation.REQUIRED)
+    public Iterable<Account> getAllAccounts() {
+        return accountRepository.findAll();
     }
 }
